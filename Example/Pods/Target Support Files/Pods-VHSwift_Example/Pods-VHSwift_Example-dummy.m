a042ae674cb18eeea1fcc6b8d4e439a0127de8b1
Vipul Dudhat <vipulp444@gmail.com>
2017-07-17 15:59:50 +0530

    Create bare project via pod create lib.

---
+++ Example/Pods/Target Support Files/Pods-VHSwift_Example/Pods-VHSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VHSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_VHSwift_Example
+@end
---
+++ Example/Pods/Target Support Files/VHSwift/VHSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VHSwift : NSObject
+@end
+@implementation PodsDummy_VHSwift
+@end
